271e8eb692c372db46d42eadf711b01b259667c9
BetoDev1 <beto.com.ar@hotmail.com>
2022-05-07 23:36:02 -0300

    update 7-May

--- js/app.js
+++ js/app.js
@@ -55,17 +55,88 @@ function consultarAPI(ciudad,pais){
     const appId='5d410194c13c3d0c9cbcf93d97d4ee1d'
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
+    Spinner()
+    debugger
+    
     fetch(url).then(respuesta => respuesta.json())
                 .then(datos => {
+
                     console.log(datos)
+
+                    limpiarHTML()
                     if (datos.cod === '404') {
                         mostrarError('Ciudad no encontrada')
                         return
                  }
 
+                 // Print answer on HTML
                  mostrarClima(datos)
 
             })
 }
 
-function mostrarClima(datos)
+function mostrarClima(datos){
+    const { name, main: {temp, temp_max, temp_min }} = datos
+    const centigrados = kelvinToCelsius(temp)
+    const max = kelvinToCelsius(temp_max)
+    const min = kelvinToCelsius(temp_min)
+
+    const nombreCiudad = document.createElement('p')
+    nombreCiudad.textContent = `Clima en ${name}`
+    nombreCiudad.classList.add('text-2xl', 'font-bold', 'text-center')
+
+    const actual = document.createElement('p')
+    actual.innerHTML = `${centigrados} &#8451`
+    actual.classList.add('font-bold', 'text-6xl')
+
+    const maxima = document.createElement('p')
+    maxima.innerHTML = `Max: ${max} &#8451`
+    maxima.classList.add('text-xl')
+
+    const minima = document.createElement('p')
+    minima.innerHTML = `Min: ${min} &#8451`
+    minima.classList.add('text-xl')
+
+    const resultadoDiv = document.createElement('div')
+    resultadoDiv.classList.add('text-center', 'text-white')
+    resultadoDiv.appendChild(nombreCiudad)
+    resultadoDiv.appendChild(actual)
+    resultadoDiv.appendChild(maxima)
+    resultadoDiv.appendChild(minima)
+
+    resultado.appendChild(resultadoDiv)
+}
+
+function kelvinToCelsius(kelvin){
+    return parseInt(kelvin - 273.15)
+}
+
+function limpiarHTML(){
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild)
+    }
+}
+
+
+function Spinner() {
+
+    limpiarHTML();
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+  
+    divSpinner.innerHTML = `
+      <div class="sk-circle1 sk-circle"></div>
+      <div class="sk-circle2 sk-circle"></div>
+      <div class="sk-circle3 sk-circle"></div>
+      <div class="sk-circle4 sk-circle"></div>
+      <div class="sk-circle5 sk-circle"></div>
+      <div class="sk-circle6 sk-circle"></div>
+      <div class="sk-circle7 sk-circle"></div>
+      <div class="sk-circle8 sk-circle"></div>
+      <div class="sk-circle9 sk-circle"></div>
+      <div class="sk-circle10 sk-circle"></div>
+      <div class="sk-circle11 sk-circle"></div>
+      <div class="sk-circle12 sk-circle"></div>
+    `;
+    resultado.appendChild(divSpinner);
+  }
